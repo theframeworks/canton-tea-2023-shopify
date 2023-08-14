256f50b2407971b6eb1a10b817650470fbba0a30
Sergio Agosti <sagosti@theframeworks.com>
2023-08-14 17:04:23 +0100

    TW defined typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   prefix: 'tw-',
   important: '#app',
@@ -26,18 +27,83 @@ module.exports = {
         16: 'repeat(16, minmax(0, 1fr))',
       },
     },
-    color: {
+    colors: {
       current: 'currentColor',
       inherit: 'inherit',
       transparent: 'transparent',
       black: '#292727',
       gray: '#4E4B48',
+      'stroke-gray': '#8F8F8F',
       white: '#ffffff',
+    },
+    fontSize: {
+      xs: [
+        '1rem',
+        {
+          lineHeight: '180%',
+          letterSpacing: '-0.04rem',
+        },
+      ],
+      sm: [
+        '1.2rem',
+        {
+          lineHeight: '120%',
+          letterSpacing: '-0.06rem',
+        },
+      ],
+      md: [
+        '1.6rem',
+        {
+          lineHeight: '130%',
+          letterSpacing: '-0.08rem',
+        },
+      ],
+      lg: [
+        '2.1rem',
+        {
+          lineHeight: '95%',
+          letterSpacing: '0',
+        },
+      ],
+      xl: [
+        '3.2rem',
+        {
+          lineHeight: '96%',
+          letterSpacing: '0',
+        },
+      ],
+      '2xl': [
+        '4.8rem',
+        {
+          lineHeight: '98%',
+          letterSpacing: '0',
+        },
+      ],
+      '3xl': [
+        '7.2rem',
+        {
+          lineHeight: '90%',
+          letterSpacing: '0',
+        },
+      ],
+      '4xl': [
+        '10rem',
+        {
+          lineHeight: '90%',
+          letterSpacing: '0',
+        },
+      ],
     },
   },
   plugins: [
     require('tailwindcss-debug-screens'),
-    function ({ addVariant, matchUtilities, theme }) {
+    function ({ addUtilities, addVariant, matchUtilities, theme }) {
+      addUtilities({
+        '.text-wrap': { 'text-wrap': 'wrap' },
+        '.text-nowrap': { 'text-wrap': 'nowrap' },
+        '.text-balance': { 'text-wrap': 'balance' },
+      });
+
       addVariant('js', ':root.js &');
       addVariant('parent', ':has(>&)');
       addVariant('not-first', '&:not(:first-child)');
