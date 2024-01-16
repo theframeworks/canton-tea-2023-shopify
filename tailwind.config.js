064e43880153717e6973b88a34e8c4f35a982399
Sergio Agosti <sagosti@theframeworks.com>
2024-01-16 17:15:52 +0000

    basic tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  prefix: 'tw-',
+  important: '#app',
+  content: ['./layout/**/*.liquid', './templates/**/*.liquid', './sections/**/*.liquid', './snippets/**/*.liquid'],
   theme: {
-    extend: {},
+    colors: {
+      current: 'currentColor',
+      inherit: 'inherit',
+      transparent: 'transparent',
+      black: '#000000',
+    },
+    screens: {
+      md: '750px',
+      lg: '990px',
+    },
+    extend: {
+      fontFamily: {
+        body: 'var(--font-body-family)',
+        heading: 'var(--font-heading-family)',
+      },
+    },
   },
   plugins: [],
-}
-
+};
